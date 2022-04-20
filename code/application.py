f19115e2eea3b789a92a900f785a7c72a5f21afd
Aaron Ho <aaronho@andrew.cmu.edu>
2022-04-20 14:25:32 -0400

    entered the clustering logic

--- code/application.py
+++ code/application.py
@@ -6,24 +6,39 @@ from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import MultiLabelBinarizer
 import altair as alt
 import streamlit as st
-from PIL import Image
 from sklearn import preprocessing
-from sklearn.linear_model import Ridge
-from sklearn.metrics import mean_squared_error
-#from sklearn.metrics import mean_absolute_error
-from sklearn.linear_model import LinearRegression
-#from sklearn.compose import TransformedTargetRegressor
-from sklearn.model_selection import train_test_split
 import cleanup as cp
-import os
-import requests
-import io
+import umap
+from sklearn.cluster import DBSCAN
+from sklearn.manifold import TSNE
 
 
 def load_data():
     # cp.prep_fbi_dataset()
     df = cp.prep_fbi_dataset()
     return df
+
+def plot_cluster(X,labels):
+    #Dimensionality Reduction
+    X_tsne2d = umap.UMAP(densmap=True,n_components=2,random_state=0).fit_transform(X)
+
+    #Clustering
+    dbscan = DBSCAN(min_samples=200, eps=1)
+    dbscan_cluster_assignments = dbscan.fit_predict(X_tsne2d)
+    inliers = dbscan_cluster_assignments != -1
+
+    #plotting the cluster
+    data = pd.concat([pd.DataFrame(X_tsne2d[:,:][inliers],columns=['X','Y']), pd.DataFrame(dbscan_cluster_assignments[inliers],columns=['Assign'])], axis=1)
+    total_counts = data['Assign'].value_counts()
+    assign = data['Assign']
+    data['total_count'] = [total_counts.get(a) for a in assign]
+    data['common_indicator'] = [labels[np.argmax(X[dbscan_cluster_assignments == i].sum(axis=0))] for i in dbscan_cluster_assignments[inliers]]
+    alt.Chart(data,title='Clustering Bias').mark_point().encode(
+        alt.X("X"),
+        alt.Y("Y"),
+        alt.Color("Assign:N",legend=alt.Legend(title="Clusters")),
+        alt.Tooltip(["Assign",'total_count','common_indicator'])
+)
     
 st.title("Application")
 with st.spinner(text="Loading data..."):
@@ -33,8 +48,34 @@ st.write(df.head())
 selection = st.selectbox("Select feature",options = ['Bias','Crime','Location'])
 
 if selection:
-    selection = [selection]
-    st.write(selection)
+    if selection == 'Bias':
+        X = df.iloc[:,75:110].to_numpy()
+        labels = df.columns[75:110]
+
+        np.random.seed(0)
+        random_sample = np.random.permutation(len(X))[:5000]
+        X = X[random_sample]
+        cluster = plot_cluster(X,labels)
+    elif selection == 'Crime':
+        X = df.iloc[:,18:75].to_numpy()
+        labels = df.columns[18:75]
+
+        np.random.seed(0)
+        random_sample = np.random.permutation(len(X))[:5000]
+        X = X[random_sample]
+        cluster = plot_cluster(X,labels)
+    elif selection == 'Location':
+        X = df.iloc[:,110:].to_numpy()
+        labels = df.columns[110:]
+
+        np.random.seed(0)
+        random_sample = np.random.permutation(len(X))[:5000]
+        X = X[random_sample]
+        cluster = plot_cluster(X,labels)
+    
+st.write(selection)
+st.write(cluster)
+
 
 
 
