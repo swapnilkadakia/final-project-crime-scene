3579a55abcd11e8d00ab8be91b9bbbd35c5d2fe4
Aaron Ho <aaronho@andrew.cmu.edu>
2022-04-20 14:30:18 -0400

    made addtional changes

--- code/application.py
+++ code/application.py
@@ -18,7 +18,22 @@ def load_data():
     df = cp.prep_fbi_dataset()
     return df
 
-def plot_cluster(X,labels):
+def plot_cluster(selection):
+    #partition dataframe depending on 
+    if selection == 'Bias':
+        X = df.iloc[:,75:110].to_numpy()
+        labels = df.columns[75:110]
+    elif selection == 'Crime':
+        X = df.iloc[:,18:75].to_numpy()
+        labels = df.columns[18:75]
+    elif selection == 'Location':
+        X = df.iloc[:,110:].to_numpy()
+        labels = df.columns[110:]
+
+    np.random.seed(0)
+    random_sample = np.random.permutation(len(X))[:5000]
+    X = X[random_sample]
+
     #Dimensionality Reduction
     X_tsne2d = umap.UMAP(densmap=True,n_components=2,random_state=0).fit_transform(X)
 
@@ -33,12 +48,14 @@ def plot_cluster(X,labels):
     assign = data['Assign']
     data['total_count'] = [total_counts.get(a) for a in assign]
     data['common_indicator'] = [labels[np.argmax(X[dbscan_cluster_assignments == i].sum(axis=0))] for i in dbscan_cluster_assignments[inliers]]
-    alt.Chart(data,title='Clustering Bias').mark_point().encode(
+    chart = alt.Chart(data,title='Clustering Bias').mark_point().encode(
         alt.X("X"),
         alt.Y("Y"),
         alt.Color("Assign:N",legend=alt.Legend(title="Clusters")),
         alt.Tooltip(["Assign",'total_count','common_indicator'])
-)
+    )
+
+    return chart
     
 st.title("Application")
 with st.spinner(text="Loading data..."):
@@ -48,30 +65,7 @@ st.write(df.head())
 selection = st.selectbox("Select feature",options = ['Bias','Crime','Location'])
 
 if selection:
-    if selection == 'Bias':
-        X = df.iloc[:,75:110].to_numpy()
-        labels = df.columns[75:110]
-
-        np.random.seed(0)
-        random_sample = np.random.permutation(len(X))[:5000]
-        X = X[random_sample]
-        cluster = plot_cluster(X,labels)
-    elif selection == 'Crime':
-        X = df.iloc[:,18:75].to_numpy()
-        labels = df.columns[18:75]
-
-        np.random.seed(0)
-        random_sample = np.random.permutation(len(X))[:5000]
-        X = X[random_sample]
-        cluster = plot_cluster(X,labels)
-    elif selection == 'Location':
-        X = df.iloc[:,110:].to_numpy()
-        labels = df.columns[110:]
-
-        np.random.seed(0)
-        random_sample = np.random.permutation(len(X))[:5000]
-        X = X[random_sample]
-        cluster = plot_cluster(X,labels)
+    cluster = plot_cluster(selection)
     
 st.write(selection)
 st.write(cluster)
